3fe316fa22475940681a1de9e552cb9571e6400a
Kim GeonHoi <jkl9510357@gmail.com>
2022-12-25 21:40:03 +0900

    ~1.4절 함수쪼개기

---
+++ chapter-01/src/1.4 함수 쪼개기/statement.js
@@ -0,0 +1,85 @@
+import invoices from "../invoices.json" assert { type: "json" };
+import plays from "../plays.json" assert { type: "json" };
+
+statement(invoices[0], plays);
+
+function statement(invoice, plays) {
+  let result = `청구내역 (고객명: ${invoice.customer})\n`;
+  for (let perf of invoice.performances) {
+    result += `${playFor(perf).name} : ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(totalAmount())}\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+
+  console.log(result);
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function volumeCreditsFor(perf) {
+    let volumeCredits = 0;
+
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === playFor(perf).type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+
+    return volumeCredits;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy": // 비극
+        result = 40000;
+
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+
+        break;
+
+      default:
+        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance)}`);
+    }
+
+    return result;
+  }
+}
